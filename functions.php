4065622d5aa9f59a6cf5108cb30232eaad1bbca3
bajra03 <putu.bajra@gmail.com>
2025-01-17 14:03:35 +0800

    initial commit

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ * @package lvenvl
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Define all constant.
+define( 'LVENVL_VERSION', '1.0.0' );
+define( 'LVENVL_URI', get_template_directory_uri() );
+define( 'LVENVL_PATH', get_template_directory() );
+define( 'LVENVL_INCLUDES', LVEVL_PATH . '/includes/' );
